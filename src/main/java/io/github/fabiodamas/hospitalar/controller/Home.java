465c7f12b9a06d27cadf874a2c167d4601a05eeb
Fábio Damas <fabio.damas@gmail.com>
2019-12-11 17:56:20 -0200

    Config controller e view for thymeleaft

--- src/main/java/io/github/fabiodamas/hospitalar/controller/Home.java
+++ src/main/java/io/github/fabiodamas/hospitalar/controller/Home.java
@@ -1,13 +1,15 @@
 package io.github.fabiodamas.hospitalar.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+@Controller
 public class Home {
 	
-	@RequestMapping("/")
-	String index() {
-		return "Servidor no ar!!!  <p> http://localhost:8080/swagger-ui.html#/ </p>";
-	}
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}	
+
+	
 }
