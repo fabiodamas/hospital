a6f4ff63a0303e95a275ec38b6aee412a75c7e78
Fábio Damas <fabio.damas@gmail.com>
2019-12-11 18:31:51 -0200

    HATEOS apis

--- src/main/java/io/github/fabiodamas/hospitalar/repository/UsuarioRepository.java
+++ src/main/java/io/github/fabiodamas/hospitalar/repository/UsuarioRepository.java
@@ -1,9 +1,11 @@
 package io.github.fabiodamas.hospitalar.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import io.github.fabiodamas.hospitalar.model.entity.Usuario;
 
+@RepositoryRestResource (collectionResourceRel="usuario",path="usuario")
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	Usuario findByNome(String nome);
 }
